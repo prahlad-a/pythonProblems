b9a7f90caefba141fa12a1f0a5ac75c1595f08f4
prahlad-a <71860930+prahlad-a@users.noreply.github.com>
2021-05-04 10:39:07 +0530

    Delete secondProb.py

--- secondProb.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter a number"))
-for i in range(n):
-    if(i==6):
-        print("We got the number")
-        print("and modified the original")
-    if(i==7)
-    {
-        print("The number is seven")
-    }
